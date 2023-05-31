9918cfc40716741f1e7933725ba182d1d9e5e95d
technoY2K <cloud@kevia.me>
2023-05-30 21:09:44 -0700

    feat: changing channels works

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,8 @@
             </h6>
         </section>
         <section
-            class="relative aspect-4/3 w-full bg-slate-500 md:h-[500px] lg:aspect-4/3 lg:w-[500px] xl:aspect-video xl:w-[750px]"
+            class="relative aspect-4/3 w-full bg-slate-500 bg-cover bg-center md:h-[500px] lg:aspect-4/3 lg:w-[500px] xl:aspect-video xl:w-[750px]"
+            :style="{ backgroundImage: `url(${currentMedia})` }"
         >
             <ul
                 class="absolute bottom-0 left-0 flex h-[64px] w-full flex-row justify-start gap-x-4 bg-secondary"
@@ -19,22 +20,30 @@
                 <li
                     class="flex h-[64px] w-[64px] cursor-pointer items-center justify-center border-4 border-stone-400 hover:bg-stone-400"
                 >
-                    13
+                    <button class="h-full w-full" @click="mediaIndex = 0">
+                        13
+                    </button>
                 </li>
                 <li
                     class="flex h-[64px] w-[64px] cursor-pointer items-center justify-center border-4 border-stone-400 hover:bg-stone-400"
                 >
-                    42
+                    <button class="h-full w-full" @click="mediaIndex = 1">
+                        42
+                    </button>
                 </li>
                 <li
                     class="flex h-[64px] w-[64px] cursor-pointer items-center justify-center border-4 border-stone-400 hover:bg-stone-400"
                 >
-                    66
+                    <button class="h-full w-full" @click="mediaIndex = 2">
+                        66
+                    </button>
                 </li>
                 <li
                     class="flex h-[64px] w-[64px] cursor-pointer items-center justify-center border-4 border-stone-400 hover:bg-stone-400"
                 >
-                    84
+                    <button class="h-full w-full" @click="mediaIndex = 3">
+                        84
+                    </button>
                 </li>
                 <li
                     class="ml-auto flex h-[64px] w-[64px] cursor-pointer items-center justify-center border-4 border-stone-400 bg-red-600 hover:bg-stone-400"
@@ -46,4 +55,13 @@
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    import advert1 from "@/assets/joneses.gif";
+    import advert2 from "@/assets/vote.gif";
+    import advert3 from "@/assets/mindless.gif";
+    import advert4 from "@/assets/home.gif";
+
+    const mediaPaths = [advert1, advert2, advert3, advert4];
+    const mediaIndex = ref(0);
+    const currentMedia = computed(() => mediaPaths[mediaIndex.value]);
+</script>
