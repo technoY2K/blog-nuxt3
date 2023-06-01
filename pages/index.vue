610a8805912b153ba743e5e20dc2910865c35a65
technoY2K <cloud@kevia.me>
2023-05-31 20:35:36 -0700

    chore: save point for the night

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div
-        class="flex flex-col gap-y-8 pt-4 md:pt-16 lg:flex-row lg:justify-around"
+        class="mb-32 flex min-h-[800px] flex-col gap-y-8 pt-4 md:pt-16 lg:flex-row lg:justify-around"
     >
         <HeroCopy />
         <TeleVision />
+    </div>
+    <div>
+        <WorkExperience />
     </div>
 </template>
