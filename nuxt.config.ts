148277dec0cb3d3ecde20eb66f615f6e9ea2629f
technoY2K <cloud@kevia.me>
2023-06-01 21:34:52 -0700

    feat: set some basic SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: "Welcome",
+            meta: [
+                {
+                    name: "description",
+                    content: "Personal website of Kevia Cloud",
+                },
+            ],
+            link: [{ rel: "canonical", href: "https://www.kevia.me" }],
+        },
+    },
     googleFonts: {
         families: {
             Poppins: true,
